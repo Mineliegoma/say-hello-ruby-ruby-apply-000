33329c5cf3218c868892826516446ab3162b0c12
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-08 22:09:25 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello (name="Hello Ruby Programmer!")
+def say_hello (name, last="Hello Ruby Programmer!")
  say_hello("person's name")
   puts "Hello #{name}"
   
